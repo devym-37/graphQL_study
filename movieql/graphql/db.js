95c6cbdc3d7ce83d0529aa5f89568059d19e18d0
YoungMin Lee <miniyoung37@gmail.com>
2020-12-07 00:42:39 +0900

    # add & delete function & schema

--- movieql/graphql/db.js
+++ movieql/graphql/db.js
@@ -1,25 +1,44 @@
-export const people = [
+let movies = [
   {
-    id: "1",
-    name: "Young",
-    age: 20,
-    gender: "male",
+    id: 1,
+    name: "Avengers",
+    score: 50,
   },
   {
-    id: "2",
-    name: "lee",
-    age: 21,
-    gender: "male",
+    id: 2,
+    name: "The Godfather",
+    scroe: 21,
   },
   {
-    id: "3",
-    name: "min",
-    age: 22,
-    gender: "female",
+    id: 3,
+    name: "Logan",
+    score: 2,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => String(id) === person.id);
-  return filteredPeople[0];
+  const filterMovies = movies.filter((movie) => id === movie.id);
+  return filterMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
+};
+
+export const addMovie = (name, score) => {
+  const newMovie = {
+    id: `${movies.length + 1}`,
+    name,
+    score,
+  };
+  movies.push(newMovie);
+  return newMovie;
 };
