dd55a8f85aca3b509b60a0d59ae94fa153c5bee5
YoungMin Lee <miniyoung37@gmail.com>
2020-12-07 23:06:22 +0900

    # REST API 적용

--- movieql/graphql/db.js
+++ movieql/graphql/db.js
@@ -1,44 +1,54 @@
-let movies = [
-  {
-    id: 1,
-    name: "Avengers",
-    score: 50,
-  },
-  {
-    id: 2,
-    name: "The Godfather",
-    scroe: 21,
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2,
-  },
-];
+// let movies = [
+//   {
+//     id: 1,
+//     name: "Avengers",
+//     score: 50,
+//   },
+//   {
+//     id: 2,
+//     name: "The Godfather",
+//     scroe: 21,
+//   },
+//   {
+//     id: 3,
+//     name: "Logan",
+//     score: 2,
+//   },
+// ];
 
-export const getMovies = () => movies;
+// export const getMovies = () => movies;
 
-export const getById = (id) => {
-  const filterMovies = movies.filter((movie) => id === movie.id);
-  return filterMovies[0];
-};
+// export const getById = (id) => {
+//   const filterMovies = movies.filter((movie) => id === movie.id);
+//   return filterMovies[0];
+// };
 
-export const deleteMovie = (id) => {
-  const cleanedMovies = movies.filter((movie) => movie.id !== id);
-  if (movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  } else {
-    return false;
-  }
-};
+// export const deleteMovie = (id) => {
+//   const cleanedMovies = movies.filter((movie) => movie.id !== id);
+//   if (movies.length > cleanedMovies.length) {
+//     movies = cleanedMovies;
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
 
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score,
-  };
-  movies.push(newMovie);
-  return newMovie;
+// export const addMovie = (name, score) => {
+//   const newMovie = {
+//     id: `${movies.length + 1}`,
+//     name,
+//     score,
+//   };
+//   movies.push(newMovie);
+//   return newMovie;
+// };
+
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json";
+
+export const getMovies = (limit, rating) => {
+  fetch(`${API_URL}`)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
 };
